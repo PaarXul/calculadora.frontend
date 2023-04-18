c73712a2cef46b98083d586a124b34c010937ed4
PaarXul <gian.parra96@gmail.com>
2023-04-17 20:47:49 -0400

    Primer Comiit

---
+++ js/calculadora.js
@@ -0,0 +1,20 @@
+// operaciones de la calculadora
+function suma(a, b) {
+    return a + b;
+}
+
+function resta(a, b) {
+    return a - b;
+}
+
+function multiplicacion(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function calculadora() {
+  
+}

4504073426916f3e559e5103a663c70a1af91dc9
PaarXul <gian.parra96@gmail.com>
2023-04-18 21:09:07 -0400

    Commit Proyecto

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,10 +1,27 @@
 const pantalla = document.getElementById("resultado");
 const btn1 = document.querySelectorAll(".btn");
-
-const btnArray = btn1.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-
-    pantalla.value = pantalla.value + e.target.value;
-
+let isResult = false
+  const btnArray = btn1.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      if(isResult) {
+        pantalla.value = btn.value
+        isResult = false
+      } else {
+        if (btn.id === "btnc"){
+          pantalla.value = ""
+          return
+        }
+        if (btn.id === "btnigual"){
+          try{
+            pantalla.value = eval(pantalla.value)
+            isResult = true
+          }catch{
+            alert('Operacion no valida')
+            pantalla.value = ''
+          }
+          return
+        }
+        pantalla.value += e.target.value;
+      }
+    });
   });
-});
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Calculadora Básica en HTML, CSS, Bootstrap y JavaScript
+
+## Descripción
+
+* Calculadora que realiza las operaciones básicas de suma, resta, multiplicación y división.
+* la calculadora realiza las operaciones con dos números.
+* reconoce los operadores +, -, X, /.
+* reconoce los números del 0 al 9.
+
+---
+
+## Como usar el repositorio de GitHub
+
+* Clonar el repositorio
+* Abrir el archivo index.html en el navegador
+* Ingresar los números y operadores
+* Presionar el botón igual para ver el resultado
+
+---
+
+### Creado por
+
+* Moises Lagos
+* Camila Concha
+* Nicolas Hernandez
+* Gian Parra
+  
+#### Copyrigth 2023
